d6a051f21da3058349fe65466c83d5ec2774222b
Uvnik <uvnik@yahoo.com>
2021-07-08 11:13:39 +0530

    added a sleep of 30 seconds before starting the job for creating buckets

--- k8s/start_platform.bat
+++ k8s/start_platform.bat
@@ -1,5 +1,8 @@
 kubectl apply -f pg_dep.yml
 kubectl apply -f minio_dep.yml
 kubectl apply -f mlflow_server.yml
+@echo off
+@timeout /t 30 /nobreak
+@echo on
 kubectl apply -f minioclient_job.yml
 kubectl apply -f mltoolkit_dep.yml
