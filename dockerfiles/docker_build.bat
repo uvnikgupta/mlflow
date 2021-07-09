24f9e2ec43b4a6aff9c0b59ddf9aec17867bb020
Uvnik <uvnik@yahoo.com>
2021-07-09 05:57:03 +0530

    Removed simple pipeline and combined all regression uses cases under one docker image. Added tensorflow to mltoolkit and linreg docker images.

--- dockerfiles/docker_build.bat
+++ dockerfiles/docker_build.bat
@@ -1,6 +1,5 @@
 docker build -t mlflow_server -f Dockerfile.mlflow.server .
 docker build -t mlflow_logreg_infer -f Dockerfile.logreg.inference .
-docker build -t mlflow_simplpipe_linreg_infer -f Dockerfile.simplepipe.inference .
-docker build -t mlflow_advpipe_linreg_infer --build-arg USER=%PACKAGR_USER% --build-arg PASS=%PACKAGR_PASS% -f Dockerfile.advancedpipe.inference .
+docker build -t mlflow_linreg_infer --build-arg USER=%PACKAGR_USER% --build-arg PASS=%PACKAGR_PASS% -f Dockerfile.linreg.inference .
 docker build -t mltoolkit -f Dockerfile.mltoolkit .
 docker build -t minioclient -f Dockerfile.minioclient .
